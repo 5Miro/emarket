3b8d01881a6c24ce58849f586f7ccf86f2f5b96c
Miro <43798115+5Miro@users.noreply.github.com>
2021-03-12 17:24:33 -0300

    Heroko deployment setup

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,14 @@ import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import dotenv from "dotenv";
 import orderRouter from "./routers/orderRouter.js";
+import path from "path";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config();
-
 
 const app = express();
 app.use(express.json());
@@ -14,20 +19,23 @@ app.use(express.urlencoded({ extended: true }));
 
 const DB = process.env.MONGODB_URL;
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(con => {
-    console.log("DB connection succesful.");
-}).catch(err => {
-    console.log("DB connection failed.")
-});
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.then((con) => {
+		console.log("DB connection succesful.");
+	})
+	.catch((err) => {
+		console.log("DB connection failed.");
+	});
 
 app.get("/api/config/paypal", (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || "sb")
-})
+	res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
 app.use("/api/users", userRouter);
 
@@ -35,16 +43,18 @@ app.use("/api/products", productRouter);
 
 app.use("/api/orders", orderRouter);
 
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+
 app.get("/", (req, res) => {
-    res.send("Server is ready");
+	res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message }); // show error for async api requests
-})
+	res.status(500).send({ message: err.message }); // show error for async api requests
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`);
-})
+	console.log(`Serve at http://localhost:${port}`);
+});
